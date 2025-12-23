c7a4b8c3fbc1d07a2e68dee815b62d5776a3df93
Aman Mishra <23053103@kiit.ac.in>
2025-12-23 21:09:33 +0530

    Implement MinSizeSubArray method with corrections
    Fix logic errors and improve variable names in MinSizeSubArray method.

---
+++ MinSizeSubArrar.java
@@ -0,0 +1,13 @@
+public int MinSizeSubArray(int target,int[] nums)
+  int leftwin=0,sum=0,minwinsize=Integer_MAX.VALUE;
+  for(int r=0;r<nums.lenth();r++){
+    sum+=nums[r];
+    while(sum>=target){
+      minwinsize=Math.min(minwinsize,r-leftwin+1)
+        sum-=nums[l]
+        l++;
+    }
+  }
+return (minwinsize==Integer_MAX.VALUE)? 0:m;
+}
+  
