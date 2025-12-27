503d494a854252f3ddb7c621f9ce88721abcf9ff
Aman Mishra <23053103@kiit.ac.in>
2025-12-28 00:20:05 +0530

    Implement pairSum1 method to find target pairs

---
+++ PairSum.java
@@ -0,0 +1,14 @@
+    public static boolean pairSum1(ArrayList<Integer> list,int target){
+        int lp=0;
+        int rp=list.size()-1;
+        while(lp<rp){
+            if(list.get(lp)+list.get(rp)==target){
+                return true;
+            }else if(list.get(lp)+list.get(rp)<target){
+                lp++;
+            }else{
+                rp--;
+            }
+        }
+        return false;
+    }
