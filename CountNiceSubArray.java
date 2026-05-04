2455d7be4cec74b8978c33625610ba5202c6bc12
Aman Mishra <23053103@kiit.ac.in>
2026-05-04 15:16:54 +0530

    Implement numberofSubarray method in Solution class

---
+++ CountNiceSubArray.java
@@ -0,0 +1,16 @@
+class Solution{
+  public int numberofSubarray(int[]nums,int k){
+    HashMap<Integer,Integer> map=new HashMap<>();
+    map.put(0,1);
+    int sum=0;
+    int ans=0;
+    for(int i=0;i<nums.length;i++){
+      sum+=(nums[i]%2);
+      if(map.containsKey(sum-k)){
+        ans+=map.get(sum-k);
+      }
+      map.put(sum,map.getOrDefault(sum,0)+1);
+    }
+    return ans;
+  }
+}
