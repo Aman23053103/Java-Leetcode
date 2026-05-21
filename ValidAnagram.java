aa51f553ec1f4181b21fa416b4729f1dabd3c787
Aman Mishra <23053103@kiit.ac.in>
2026-05-21 22:01:39 +0530

    Implement isAnagram method to check for anagrams

---
+++ ValidAnagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       HashMap<Character,Integer> hm=new HashMap<>();
+       if(s.length()!=t.length()){
+        return false;
+       }
+       for(int i=0;i<s.length();i++){
+        hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
+       }
+       for(int i=0;i<t.length();i++){
+        char ch=t.charAt(i);
+        if(hm.containsKey(ch)){
+            if(hm.get(ch)==1){
+                hm.remove(ch);
+            }else{
+                hm.put(ch,hm.get(ch)-1);
+            }
+        }else{
+            return false;
+        }
+       }
+       return hm.isEmpty();
+    }
+}
