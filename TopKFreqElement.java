66100eef92bac16bb3e76f75fd6b7c3a6bb7f136
Aman Mishra <23053103@kiit.ac.in>
2026-05-28 18:48:15 +0530

    Refactor topKFrequent method for clarity and efficiency

--- TopKFreqElement.java
+++ TopKFreqElement.java
@@ -1,26 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        List<Integer>[] bucket=new List[nums.length+1];
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int n : nums){
-            hm.put(n,hm.getOrDefault(n,0)+1);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        for(int key: hm.keySet()){
-            int freq=hm.get(key);
-            if(bucket[freq]==null){
-                bucket[freq]=new ArrayList<>();
-            }
-            bucket[freq].add(key);
-        }
-        int[] res=new int[k];
-        int counter=0;
-        for(int pos=bucket.length-1;pos>=0 && counter<k;pos--){
-            if(bucket[pos]!=null){
-                for(Integer integer : bucket[pos]){
-                    res[counter++]=integer;
-                }
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->map.get(a)-map.get(b));
+        for(int num:map.keySet()){
+            pq.add(num);
+            if(pq.size()>k){
+                pq.remove();
             }
         }
-        return res;
+        int[] ans=new int[k];
+        for(int i=k-1;i>=0;i--){
+            ans[i]=pq.remove();
+        }
+        return ans;
     }
 }
