2e8564bbea0b1522e6a557a363294040d9a8881c
Aman Mishra <23053103@kiit.ac.in>
2026-05-29 20:16:07 +0530

    Implement nextGreaterElements method in Solution class

---
+++ NextGreaterElement2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+       Stack<Integer> st=new Stack<>();
+       int n=nums.length;
+       int[] ans=new int[n];
+       for(int i=2*n-1;i>=0;i--){
+        int index=i%n;
+        while(!st.isEmpty() && st.peek()<=nums[index]){
+            st.pop();
+        }
+        if(i<n){
+            if(st.isEmpty()){
+                ans[index]=-1;
+            }else{
+                ans[index]=st.peek();
+            }
+        }
+        st.push(nums[index]);
+       } 
+       return ans;
+    }
+}
